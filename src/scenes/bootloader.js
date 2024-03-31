7662be706605b351397d1966794ad0351579a5d8
DaniVaci18 <loljose1090@gmail.com>
2024-03-31 02:11:29 -0500

    feature: assets create in pong scene

--- src/scenes/bootloader.js
+++ src/scenes/bootloader.js
@@ -1,25 +1,25 @@
 class SceneBootLoader extends Phaser.Scene {
-    constructor () {
-        super({
-            key:"SceneBootLoader"
-        })
-    }
+  constructor() {
+    super({
+      key: "SceneBootLoader",
+    });
+  }
 
-    preload(){
-        this.load.image("ball", "./resource/pong-assets/ball.png");
-        this.load.image("ball", "./resource/pong-assets/left_pallete.png");
-        this.load.image("ball", "./resource/pong-assets/right_pallete.png");
-        this.load.image("ball", "./resource/pong-assets/separator.png");
-    }
+  preload() {
+    //!!!IMPORTANTE
+    //Metodo que indica que ya se cargó toda está escena por completo.
+    this.load.on("complete", () => {
+        this.scene.start("ScenePong");
+    })
+    //Ya que estos archivos son llamados asincronos como de promesas se tratasen
+    this.load.image("ball", "./resource/pong-assets/ball.png");
+    this.load.image("leftPallete", "./resource/pong-assets/leftPallete.png");
+    this.load.image("rightPallete", "./resource/pong-assets/rightPallete.png");
+    this.load.image("separator", "./resource/pong-assets/separator.png");
+  }
 
-    create(){
-        console.log("SceneBootLoader");
-    }
-
-    update(){
-
-    }
-
+  create() {}
+  update() {}
 }
 
-export default SceneBootLoader
+export default SceneBootLoader;
--- src/scenes/pong.js
+++ src/scenes/pong.js
@@ -1,16 +1,42 @@
+import { getRandomInt } from "../utils/complemets";
+
 class ScenePong extends Phaser.Scene {
   constructor() {
     super({ key: "ScenePong" });
   }
 
-  preload() {
-  }
+  preload() {}
 
   create() {
+    const widthGame = this.game.config.width;
+    const heightGame = this.game.config.height;
+
+    //Separator, load in SceneBootLoader
+    this.separator = this.add.image(widthGame / 2, heightGame / 2, "separator");
+    this.separator.setScale(1, 1.2);
+
+    let distanceFromSides = 30;
+
+    this.leftPallete = this.add.image(
+      distanceFromSides,
+      heightGame / 2,
+      "leftPallete"
+    );
+    
+    this.rightPallete = this.add.image(
+      widthGame - distanceFromSides,
+      heightGame / 2,
+      "rightPallete"
+    );
+
+    this.ball = this.add.image(
+      getRandomInt(distanceFromSides, widthGame - distanceFromSides),
+      getRandomInt(0, heightGame),
+      "ball"
+    );
+
     console.log("Creando PONG");
   }
-
-
 }
 
 export default ScenePong;
